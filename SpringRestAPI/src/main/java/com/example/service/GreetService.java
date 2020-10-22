872301992bc138d68891eca2fef854dbde98ba9f
99002761 <72496204+99002761@users.noreply.github.com>
2020-10-22 17:49:53 +0530

    Add files via upload

---
+++ SpringRestAPI/src/main/java/com/example/service/GreetService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+public interface GreetService {
+	
+	//String showMessage1();
+	
+	String welcomeUser(String name);
+	String printName(String name);
+
+	String showMessage();
+
+}
